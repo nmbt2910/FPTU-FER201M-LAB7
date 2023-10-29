4160dbc88d80404df8b8d2be47dd531f777f0193
fantacola0x0 <96630960+fantacola0x0@users.noreply.github.com>
2023-10-29 17:52:24 +0700

    hello2

--- src/AddPlayerForm.js
+++ src/AddPlayerForm.js
@@ -1,44 +1,9 @@
+// eslint-disable-next-line
+import * as React from 'react';
+// eslint-disable-next-line
 import * as Yup from 'yup';
 
 function AddPlayerForm() {
-  const formik = useFormik({
-    initialValues: {
-      nation: '',
-      club: '',
-      cost: 0,
-      clip: '',
-      description: '',
-      img: '',
-      top: false,
-    },
-    validationSchema: Yup.object({
-      nation: Yup.string().required('Required.').min(2, 'Must be 2 characters or more'),
-      club: Yup.string().required('Required.').min(2, 'Must be 2 characters or more'),
-      cost: Yup.number().required('Required.').integer().typeError('Please type a number.'),
-      clip: Yup.string().required('Required.').min(10, 'Must be 10 characters or more'),
-      description: Yup.string().required('Required.').min(10, 'Must be 10 characters or more'),
-      img: Yup.string().required('Required.').min(10, 'Must be 10 characters or more'),
-    }),
-    onSubmit: (values) => {
-      const baseURL = 'https://6535dff3c620ba9358ecb96d.mockapi.io/dbPlayers'; // Replace with your API endpoint URL
-
-      fetch(baseURL, {
-        method: 'POST',
-        body: JSON.stringify(values),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP Status: ${response.status}`);
-          }
-          console.log('Player added successfully!');
-        })
-        .catch(error => console.log(error.message));
-    },
-  });
-
   return null;
 }
 
